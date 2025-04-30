354178ac2ccacb0ce9ec660ad6972098d00c5727
RicardoFrieza <a71483@ualg.pt>
2025-04-30 02:41:25 +0100

    Added basic initial movement

--- Assets/Game/Scripts/Character/PlayerFighter.cs
+++ Assets/Game/Scripts/Character/PlayerFighter.cs
@@ -1,10 +1,33 @@
+using UnityEngine;
+
 public class PlayerFighter : Fighter
 {
+    [SerializeField] private float moveSpeed = 5f;
+
+    private Vector3 moveDir;
+
     protected override void ReadInput()
     {
+        Vector2 inputVec = input.MoveVector;
+        moveDir = new Vector3(inputVec.x, 0f, inputVec.y);
     }
 
     protected override void HandleMovement()
     {
+        float originalY = transform.position.y;
+
+        if (moveDir.sqrMagnitude > 0.01f)
+        {
+            controller.Move(moveDir.normalized * moveSpeed * Time.deltaTime);
+            transform.forward = moveDir.normalized;
+        }
+
+        Vector3 fix = transform.position;
+        fix.y = originalY;
+        transform.position = fix;
+
+        //if (!controller.isGrounded)
+        //    controller.Move(Physics.gravity * Time.deltaTime);
+        //    Debug.Log(controller.isGrounded);       
     }
 }
